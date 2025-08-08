589e620cba23c99ea0ff1d08789416da230b08e3
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-08-07 19:16:29 -0600

    Opciones

--- Express.py
+++ Express.py
@@ -6,3 +6,24 @@ def Menu():
     print("4.Buscar repartidor")
     print("5.Estadisticas generales")
     print("6.Salir")
+allow = False
+while allow == False:
+    Menu()
+    opt = int(input("Ingrese la opción que desee: "))
+    print(" ")
+    match opt:
+        case 1:
+            num = int(input("Ingrese cuantos repartidores desea agregar: "))
+        case 2:
+            print("Mostrar")
+        case 3:
+            print("Mostrar")
+        case 4:
+            print("Buscar")
+        case 5:
+            print("Stats")
+        case 6:
+            print("Gracias por utilizar el programa")
+            break
+        case _:
+            print("La opción seleccionada no es valida")
