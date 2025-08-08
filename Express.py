43970fa5b3181ad5e0b455e1c67b076779770476
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-08-07 20:39:40 -0600

    Arreglando ingreso y Mostrar desordenado

--- Express.py
+++ Express.py
@@ -3,11 +3,16 @@ class Messenger:
         self.name = name
         self.pack = pack
         self.zone = zone
+    def __str__(self):
+        return f'Nombre:{self.name} - Entregas:{self.pack} - Zona:{self.zone}'
 class Pack_Buissnes:
     def __init__(self):
         self.mess = []
     def add_mess(self,messenger):
         self.mess.append(messenger)
+    def Show(self):
+        for messenger in self.mess:
+            print(messenger.__str__())
 def Menu():
     print("Paquetería BingBong")
     print("1.Ingresar repartidores: ")
@@ -17,6 +22,8 @@ def Menu():
     print("5.Estadisticas generales")
     print("6.Salir")
 allow = False
+allow1 = False
+Buissnes = Pack_Buissnes()
 names = []
 while allow == False:
     Menu()
@@ -40,14 +47,28 @@ while allow == False:
                         else:
                             zone = input("Ingrese la zona del repartidor: ")
                             new_mess = Messenger(name,pack,zone)
+                            Buissnes.add_mess(new_mess)
+                            allow1 = True
         case 2:
-            print("Mostrar")
+            if allow1 == False:
+                print("Aún no hay datos en el sistema")
+            else:
+                Buissnes.Show()
         case 3:
-            print("Mostrar")
+            if allow1 == False:
+                print("Aún no hay datos en el sistema")
+            else:
+                print(" ")
         case 4:
-            print("Buscar")
+            if allow1 == False:
+                print("Aún no hay datos en el sistema")
+            else:
+                print(" ")
         case 5:
-            print("Stats")
+            if allow1 == False:
+                print("Aún no hay datos en el sistema")
+            else:
+                print(" ")
         case 6:
             print("Gracias por utilizar el programa")
             break
