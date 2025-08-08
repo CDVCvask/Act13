56d1b4d6b425e81e388014e41758101105c1ded4
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-08-07 19:09:51 -0600

    Menu

---
+++ Express.py
@@ -0,0 +1,8 @@
+def Menu():
+    print("Paquetería BingBong")
+    print("1.Ingresar repartidores: ")
+    print("2.Mostrar repartidores desordenados")
+    print("3.Mostrar repartidores ordenados")
+    print("4.Buscar repartidor")
+    print("5.Estadisticas generales")
+    print("6.Salir")
