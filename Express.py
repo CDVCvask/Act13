74ec5b52358a5e0233b946789f26d8667e8ca4c2
CDVCvask <143233430+CDVCvask@users.noreply.github.com>
2025-08-07 19:56:31 -0600

    Ingreso

--- Express.py
+++ Express.py
@@ -1,3 +1,13 @@
+class Messenger:
+    def __init__(self,name,pack,zone):
+        self.name = name
+        self.pack = pack
+        self.zone = zone
+class Pack_Buissnes:
+    def __init__(self):
+        self.mess = []
+    def add_mess(self,messenger):
+        self.mess.append(messenger)
 def Menu():
     print("Paquetería BingBong")
     print("1.Ingresar repartidores: ")
@@ -7,6 +17,7 @@ def Menu():
     print("5.Estadisticas generales")
     print("6.Salir")
 allow = False
+names = []
 while allow == False:
     Menu()
     opt = int(input("Ingrese la opción que desee: "))
@@ -14,6 +25,21 @@ while allow == False:
     match opt:
         case 1:
             num = int(input("Ingrese cuantos repartidores desea agregar: "))
+            if num <= 0:
+                print("El valor ingresado no es valido")
+            else:
+                for i in range (num):
+                    name = input("Ingrese el nombre del repartidor: ")
+                    if name in names:
+                        print("No se puede repetir el nombre")
+                    else:
+                        names.append(name)
+                        pack = int(input("Cuantos paquetes entregó? "))
+                        if pack <= 0:
+                            print("El valor ingresado no es valido")
+                        else:
+                            zone = input("Ingrese la zona del repartidor: ")
+                            new_mess = Messenger(name,pack,zone)
         case 2:
             print("Mostrar")
         case 3:
